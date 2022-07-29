edc8c8889b7be5e4e99dc294575dd4f5018f2242
kitbenjamin <53219529+kitbenjamin@users.noreply.github.com>
2022-07-29 09:50:46 +0100

    fix package build

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ Created on Wed Apr  6 09:06:24 2022
 
 from distutils.core import setup
 
-setup(name='ADMSQpy',
+setup(name='BHAQpy',
       version='1.0',
-      description='Seemless integration between QGIS and ADMS through python',
+      description='Seemless integration of technical AQ work and python',
       author='Kit Benjamin',
       author_email='kitbenjamin@googlemail.com',
-      url='https://github.com/kitbenjamin/ADMSQpy',
-      packages=['ADMSQpy'],
+      url='https://github.com/BH-air-quality/BHAQpy',
+      packages=['BHAQpy'],
      )
